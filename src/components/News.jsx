8110ecbb0269933d617efab6def8b59e3f73175e
yubinex <janhauck99@gmail.com>
2025-03-01 13:54:57 +0100

    news section styling

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -1,4 +1,10 @@
+import healthImg from "../assets/images/health.jpg";
+import nationImg from "../assets/images/nation.jpg";
+import scienceImg from "../assets/images/science.jpg";
+import sportsImg from "../assets/images/sports.jpg";
+import techImg from "../assets/images/tech.jpg";
 import userImg from "../assets/images/user.jpg";
+import worldImg from "../assets/images/world.jpg";
 import Calendar from "./Calendar";
 import "./News.css";
 import Weather from "./Weather";
@@ -61,8 +67,58 @@ const News = () => {
                     </div>
                 </div>
                 <div className="news-section">
-                    <div className="headline">Headline</div>
-                    <div className="news-grid">News Grid</div>
+                    <div className="headline">
+                        <img src={techImg} alt="Headline Image" />
+                        <h2 className="headline-title">
+                            Lorem ipsum dolor sit amet consectetur adipisicing
+                            elit.
+                            <i className="fa-regular fa-bookmark bookmark" />
+                        </h2>
+                    </div>
+                    <div className="news-grid">
+                        <div className="news-grid-item">
+                            <img src={techImg} alt="News Image" />
+                            <h3>
+                                Lorem ipsum dolor sit amet.
+                                <i className="fa-regular fa-bookmark bookmark"></i>
+                            </h3>
+                        </div>
+                        <div className="news-grid-item">
+                            <img src={sportsImg} alt="News Image" />
+                            <h3>
+                                Lorem ipsum dolor sit amet.
+                                <i className="fa-regular fa-bookmark bookmark"></i>
+                            </h3>
+                        </div>
+                        <div className="news-grid-item">
+                            <img src={scienceImg} alt="News Image" />
+                            <h3>
+                                Lorem ipsum dolor sit amet.
+                                <i className="fa-regular fa-bookmark bookmark"></i>
+                            </h3>
+                        </div>
+                        <div className="news-grid-item">
+                            <img src={worldImg} alt="News Image" />
+                            <h3>
+                                Lorem ipsum dolor sit amet.
+                                <i className="fa-regular fa-bookmark bookmark"></i>
+                            </h3>
+                        </div>
+                        <div className="news-grid-item">
+                            <img src={healthImg} alt="News Image" />
+                            <h3>
+                                Lorem ipsum dolor sit amet.
+                                <i className="fa-regular fa-bookmark bookmark"></i>
+                            </h3>
+                        </div>
+                        <div className="news-grid-item">
+                            <img src={nationImg} alt="News Image" />
+                            <h3>
+                                Lorem ipsum dolor sit amet.
+                                <i className="fa-regular fa-bookmark bookmark"></i>
+                            </h3>
+                        </div>
+                    </div>
                 </div>
                 <div className="my-blogs">My Blogs</div>
                 <div className="weather-calendar">
